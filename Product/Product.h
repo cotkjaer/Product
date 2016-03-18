1adbc12a4125f025e00222d4971f2fe422cc9097
christian otkjær <christian.otkjaer@gmail.com>
2016-03-18 20:19:19 +0100

    Initial Commit

---
+++ Product/Product.h
@@ -0,0 +1,19 @@
+//
+//  Product.h
+//  Product
+//
+//  Created by Christian Otkjær on 18/03/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Product.
+FOUNDATION_EXPORT double ProductVersionNumber;
+
+//! Project version string for Product.
+FOUNDATION_EXPORT const unsigned char ProductVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Product/PublicHeader.h>
+
+
